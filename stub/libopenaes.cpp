238d2918181374b41abe21c399d3ea15a53a797f
xlwnx <alwanfarid19@gmail.com>
2026-01-06 16:35:54 +0700

    update

--- stub/libopenaes.cpp
+++ stub/libopenaes.cpp
@@ -1,4 +1,20 @@
-// Libopenaes Stub for TWRP Build
+#include <stdlib.h>
+
+// Definisi fungsi dummy OpenAES agar linker tidak error
 extern "C" {
-    void openaes_stub() {}
+    void* oaes_alloc() {
+        return NULL;
+    }
+
+    int oaes_key_import_data(void* ctx, const unsigned char* data, size_t len) {
+        return 0;
+    }
+
+    int oaes_decrypt(void* ctx, const unsigned char* m, size_t m_len, unsigned char* c, size_t* c_len) {
+        return 0;
+    }
+
+    int oaes_free(void** ctx) {
+        return 0;
+    }
 }
